25c4065f33d89e172e314f36dea1b5c294473c33
Layc <youareastar11@gmail.com>
2019-01-21 21:52:42 -0700

    re commiting my reposity

---
+++ script.js
@@ -0,0 +1,13 @@
+<<<<<<< HEAD
+var petOwner = {};
+  petOwner.name = "";
+  petOwner.winnerResults = null;
+  petOwner.totalVotes = 0;
+
+  var 
+=======
+var politician = {};
+  politician.name= "";
+  politician.electionResults = null;
+  politician.totalVotes = 0;
+>>>>>>> 31ad6d67f992b4941881d042c849811ce3d886c8

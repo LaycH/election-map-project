ef68692a746c65b916c2cbb5fab7ce79abd224e6
Layc <youareastar11@gmail.com>
2019-01-25 14:45:24 -0700

    added a Method w/a For Loop to petOwner object

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 var createOwner = function (owner) {
 
-var petOwner = {};  // new blank object created!
-  petOwner.name = "";  //set the name property to the value of the (owner) parameter
+var petOwner = {};  //new blank object created!*/
+  petOwner.name = "";  //set the name property to the value of the (owner) parameter*/
   petOwner.ownerResults = null;
   petOwner.totalPets = 0;
+
+  petOwner.countTotalPets =function() /* check BONUS ELECTION MAP: WRITE YOUR JAVASCRIPT (PART 2)
+  STEP 3 OF 4: WRITE A METHOD FOR TALLYING UP YOUR RESULTS for where to put this/ method for
+   tallying votes, add to factory function above return statement*/ 
+  {
+    this.totalPets = 0;
+    
+    for (var i=0; i < this.ownerRestults.length; i ++)
+    {
+      this.totalPets = this.totalPets + this.ownerResults [i];
+    console.log(this.totalPets);
+  }
+  };
 
   return petOwner();
 
@@ -18,6 +31,7 @@ catOwners1.ownerResults = [5, 1, 7, 2, 33, 6, 4, 2, 1, 14, 8, 3, 1,
 dogOwners2.ownerResults = [4, 2, 4, 4, 22, 3, 3, 1, 2, 15, 8, 1, 3,
    9, 0, 6, 1, 5, 5, 1, 3, 7, 8, 1, 3, 3, 1, 3, 2, 2, 6, 2, 14, 0, 1, 6, 7, 3, 7, 3, 6, 1, 3, 17, 3, 1, 2, 11, 2, 3, 1];
 
+   //updated election array to follow this note*/
   catOwners1.ownerResults[9] = 1;
   dogOwners2.ownerResults[9] = 28;
 
@@ -26,3 +40,11 @@ dogOwners2.ownerResults = [4, 2, 4, 4, 22, 3, 3, 1, 2, 15, 8, 1, 3,
 
   catOwners1.ownerResutls[43] = 11;
   dogOwners2.ownerResults[43] = 27;
+
+//calling the method for each politician, add below the code that updates the electionResults arrays*/
+  catOwners1.countTotalPets();
+  dogOwners2.countTotalPets();
+
+  console.log(catOwners1.totalPets);
+  console.log(dogOwners2.totalPets);
+

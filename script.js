456f00b3cc671e01260c6fad417c1a6b11e0b647
Layc <youareastar11@gmail.com>
2019-01-26 16:59:19 -0700

    added parameter animalColor

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-var createOwner = function (owner) {
+/*updated factory function parameters add color*/
+var createOwner = function (owner,animalColor) {
 
 var petOwner = {};  //new blank object created!*/
   petOwner.name = "";  //set the name property to the value of the (owner) parameter*/
   petOwner.ownerResults = null;
   petOwner.totalPets = 0;
+  petOwner.animalColor = animalColor;
+  
 
-  petOwner.countTotalPets =function() /* check BONUS ELECTION MAP: WRITE YOUR JAVASCRIPT (PART 2)
+  petOwner.countTotalPets =function( ) /* check BONUS ELECTION MAP: WRITE YOUR JAVASCRIPT (PART 2)
   STEP 3 OF 4: WRITE A METHOD FOR TALLYING UP YOUR RESULTS for where to put this/ method for
    tallying votes, add to factory function above return statement*/ 
   {
@@ -18,12 +21,15 @@ var petOwner = {};  //new blank object created!*/
   }
   };
 
-  return petOwner();
+  return createOwner();
 
 };
 
 var catOwners1= createOwner ("Cat Owners");  
 var dogOwners2 = createOwner ("Dog Owners");
+
+var catOwners1 = createOwner ("Cat Owners", [245, 141, 136]);
+var dogOwners2 = createOwner ("Dog Owners" , [132,17, 11]);
 
 catOwners1.ownerResults = [5, 1, 7, 2, 33, 6, 4, 2, 1, 14, 8, 3, 1,
   11, 11, 0, 5, 3, 3, 3, 7, 4, 8, 9, 3, 7, 2, 2, 4, 2, 8, 3, 15, 15, 2, 12, 0, 4, 13, 1, 3, 2, 8, 21, 3, 2, 11, 1, 3, 7, 2];
@@ -47,6 +53,9 @@ dogOwners2.ownerResults = [4, 2, 4, 4, 22, 3, 3, 1, 2, 15, 8, 1, 3,
 
   console.log(catOwners1.totalPets);
   console.log(dogOwners2.totalPets);
+
+  console.log("Cat Owners color is:" + catOwners1.animalColor)
+  console.log("Dog Owners color is:" + dogOwners2.animalColor)
 
   /*subsitute "winner" with "mostly" and "winner name" for "Pet Owners"*/
 
